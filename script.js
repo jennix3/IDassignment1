a8cf8d58e4abf3b29102f27c977cf9278f3cf4f9
jennix3 <s10262629@connect.np.edu.sg>
2023-12-01 02:58:32 +0800

    final changes

--- script.js
+++ script.js
@@ -7,29 +7,28 @@ function fadeIn() {
     body.style.opacity = 1;
 }
 
-let slideIndex = 0;
-
-function showSlides() {
-    let i;
-    const slides = document.getElementsByClassName("mySlides");
-    
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    
-    slideIndex++;
-    
-    if (slideIndex > slides.length) {
-        slideIndex = 1;
-    }
-    
-    slides[slideIndex - 1].style.display = "block";
-    setTimeout(showSlides, 2000); // Change slide every 2 seconds
-}
+var slideIndex = 0;
+showSlides(slideIndex);
 
 function plusSlides(n) {
     showSlides(slideIndex += n);
 }
 
-// Initial call to start the slideshow
-showSlides();
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+
+    if (n >= slides.length) {
+        slideIndex = 0;
+    }
+
+    if (n < 0) {
+        slideIndex = slides.length - 1;
+    }
+
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+
+    slides[slideIndex].style.display = "block";
+}
